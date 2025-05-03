5b71436c15db8449fbaee561ba94713e795dde6e
Pedronovaes1 <pedro.novaes@souunit.com.br>
2025-05-03 18:15:31 -0300

    refactor: change in get_all function

--- app/routers/sentimento.py
+++ app/routers/sentimento.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import func
 from .. import models, schemas
 from ..database import get_db
-from ..services import services_sentimento
+from ..services import services_sentimentos
 
 router = APIRouter(
     prefix="",
@@ -29,7 +29,7 @@ def get_sentimentos(db: Session = Depends(get_db)):
     """
     Recupera todos os sentimentos.
     """
-    return services_sentimento.get_sentimentos(db)
+    return services_sentimentos.get_sentimentos(db)
 
 # GET /sentimentosRecorrentes
 @router.get("/sentimento/recorrente")
@@ -37,7 +37,7 @@ def sentimentos_recorrentes(db: Session = Depends(get_db)):
     """
     Recupera todos os sentimentos recorrentes.
     """
-    return services_sentimento.sentimentos_recorrentes(db)
+    return services_sentimentos.sentimentos_recorrentes(db)
 
 # GET /sentimento/tecnico/{id}
 @router.get("/sentimento/tecnico/{id}")
@@ -45,7 +45,7 @@ def get_sentimento_by_tecnico(id: int, db: Session = Depends(get_db)):
     """
     Recupera todos os sentimentos de um técnico.
     """
-    return services_sentimento.get_sentimentos_por_id(id,db)
+    return services_sentimentos.get_sentimentos_por_id(id,db)
 
 # GET /atendimento
 @router.get("/atendimento")
@@ -53,7 +53,7 @@ def get_atendimento(db: Session = Depends(get_db)):
     """
     Recupera as informações de atendimento incluindo conversas, sentimentos, atendentes e clientes.
     """
-    return services_sentimento.get_atendimento(db)
+    return services_sentimentos.get_atendimento(db)
 
 # GET /tecnico/{id}
 @router.get("/tecnico/{id}")
@@ -61,7 +61,7 @@ def get_tecnico(id: int, db: Session = Depends(get_db)):
     """
     Recupera informações de um técnico específico.
     """
-    tecnico = services_sentimento.get_tecnico(id,db)
+    tecnico = services_sentimentos.get_tecnico(id,db)
     if not tecnico:
         raise HTTPException(status_code=404, detail="Técnico não encontrado") 
     return tecnico
@@ -72,7 +72,7 @@ def get_cliente(id: int, db: Session = Depends(get_db)):
     """
     Recupera informações de um cliente específico.
     """
-    cliente = services_sentimento.get_cliente(id, db)
+    cliente = services_sentimentos.get_cliente(id, db)
     if not cliente:
         raise HTTPException(status_code=404, detail="Cliente não encontrado")
     return cliente
