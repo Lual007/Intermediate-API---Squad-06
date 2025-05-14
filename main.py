5d0adcc059b8173e3f2491b7767df81d160be75a
DaviGN <davignascimento@proton.me>
2025-05-14 19:43:08 -0300

    fix: new cors url

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ origins=[
     "http://localhost:8080",
     "http://127.0.0.1",
     "http://127.0.0.1:8080",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 app.add_middleware(
     CORSMiddleware,
